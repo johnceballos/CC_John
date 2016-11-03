2a8e0e45461c7005c0f55087db430035211bc8a3
johnceballos <john.ceballos@aol.com>
2016-11-03 11:56:33 -0400

    ss6 update

--- SS6_pokeClementines_johnCeballos/sketch.js
+++ SS6_pokeClementines_johnCeballos/sketch.js
@@ -11,32 +11,37 @@ function preload(){//to load image
 }
 
 function setup() {
-  createCanvas(500,500);
+  createCanvas(displayWidth,displayHeight);
   imageMode(CENTER);
 }
 
 function draw() {
   background(171,232,230);
   image(img,x,y,100,100);
+  move(x,y,speed1,speed2)
+}
+function move(){
+  
   x = x+ speed1;
   y = y+ speed2;
+  
   if ((x >= (width - 50))) {
     x = width -50;
     speed1 = (speed1 * -1);
   }
-  if (x <= 50) {
+  else if (x <= 50) {
     x = 50;
     speed1 = (speed1 * -1);
   }
-  if ((y >= (height-50))) {
+  else if ((y >= (height-50))) {
     y = height -50;
     speed2 = (speed2 * -1);
   }
-  if (y<=50) {
+  else if (y<=50) {
     y = 50;
     speed2 = (speed2 * -1);
   }
-  if (x>mouseX-50 && x<mouseX+50 && y>mouseY-50 && y<mouseY+50){
+  else if (x>mouseX-50 && x<mouseX+50 && y>mouseY-50 && y<mouseY+50){
     speed1 = (speed1 * -1);
     speed2 = (speed2 * -1);
   }
